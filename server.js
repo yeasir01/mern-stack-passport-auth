54a0c0ee1ba37f1f3f00cd3d02f7e8a1e418a597
Yeasir H <yeasir01@gmail.com>
2020-06-30 20:55:21 -0700

    refactor

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('./config/passport');
-//const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -16,13 +15,13 @@ if (process.env.NODE_ENV === 'production') {
     let logger = require('morgan');
     require('dotenv').config();
     app.use(logger('dev'));
-} 
+}
 
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false
-  }))
+}))
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -39,11 +38,6 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mern-pass-auth'
     console.log('There was an issue connecting to MongoDB')
 });
 
-/* app.get('*', (req, res) => {
-    res.sendFile(path(__dirname, '/client/build/index.html'));
-}); */
-
-//app.use(require("./routes/static"));
 app.use("/api/auth", require("./routes/api/auth"));
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`));
