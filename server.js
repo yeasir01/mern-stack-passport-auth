a1166259f79f8a348055a3517b0231e580b0494d
Yeasir H <yeasir01@gmail.com>
2020-07-21 21:10:49 -0700

    modified session options for better security

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const MongoStore = require('connect-mongo')(session);
+const setup = require('./config/setup.json')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const production = app.get('env') === 'production';
 
-if (process.env.NODE_ENV !== 'production') {
-    const logger = require('morgan');
+const MONGO_URL = production ? process.env.MONGODB_URI : 'mongodb://localhost/mern-pass-auth';
+
+if (app.get('env') !== 'production') {
+    let logger = require('morgan');
     require('dotenv').config();
     app.use(logger('dev'));
 }
@@ -18,24 +22,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.enable('trust proxy');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mern-pass-auth', {
+mongoose.connect(MONGO_URL, {
     useNewUrlParser: true, 
     useUnifiedTopology: true, 
     useCreateIndex: true,
     useFindAndModify: false
-}).then(() => {
-    console.log('Sucessfully connected to MongoDB!')
-}).catch(err => {
-    console.log('There was an issue connecting to MongoDB')
-});
+})
+.then(() => console.log('Sucessfully connected to MongoDB!'))
+.catch(err => console.log('There was an issue connecting to MongoDB'));
 
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
+    cookie: {
+        secure: production ? true : false,
+        maxAge: setup.auth.sessionTime * 1000 * 60
+    },
     store: new MongoStore({ 
         mongooseConnection: mongoose.connection,
-        ttl: 24 * 60 * 60 //time to store cookies
+        ttl: setup.auth.sessionTime * 1000 * 60
     })
 }));
 
@@ -44,8 +50,7 @@ app.use(passport.session());
 
 app.use("/api/auth", require("./routes/api/auth"));
 
-
-if (process.env.NODE_ENV === 'production') {
+if (app.get('env') === 'production') {
     app.use(express.static("client/build"));
 
     app.get('*', (req, res) => {
